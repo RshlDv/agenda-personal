7aa68278bc7f120cd7fd857285dd928def9484c7
Rashel <rashelroadiaz005@gmail.com>
2026-07-05 12:36:26 -0400

    chore: estructura del proyecto

---
+++ src/test/java/com/agenda/agenda_personal/AgendaPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
