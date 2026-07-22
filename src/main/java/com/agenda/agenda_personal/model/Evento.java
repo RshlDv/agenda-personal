38a34d6c74197ca0e5cb97bfb7966094a58e3de1
Rashel <rashelroadiaz005@gmail.com>
2026-07-22 18:59:44 -0400

    fix: se soluciona la advertencia que salía de Lombok con @Builder.Default en Evento.java

--- src/main/java/com/agenda/agenda_personal/model/Evento.java
+++ src/main/java/com/agenda/agenda_personal/model/Evento.java
@@ -1,9 +1,21 @@
 package com.agenda.agenda_personal.model;
 
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "eventos")
@@ -33,6 +45,7 @@ public class Evento {
 
     private String nota;
 
+    @Builder.Default
     private boolean completado = false;
 
     //A los sin Etiquetar
